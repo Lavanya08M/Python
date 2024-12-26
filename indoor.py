57279a6d83151d992df7b0701da1e791c17105e7
Lavanya08M <139261589+Lavanya08M@users.noreply.github.com>
2024-12-26 18:40:38 -0500

    Update indoor.py
    added comments to the program

--- indoor.py
+++ indoor.py
@@ -1,6 +1,11 @@
 def main():
+    # Ask the user to enter anything and store it in user_input variable
     user_input = input()
+
+    # Print the user input in upper case
     print(user_input.upper())
+
+    # Print the user input in lower case
     print(user_input.lower())
 
 if __name__ == "__main__":
