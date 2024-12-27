fc7e2d019f6158e164d983347a953953adc0440b
Lavanya08M <139261589+Lavanya08M@users.noreply.github.com>
2024-12-26 19:31:47 -0500

    Update playback.py
    Added comments

--- playback.py
+++ playback.py
@@ -1,6 +1,11 @@
 def main():
+    # Take the user input
     user_input = input()
+
+    # Replace the space in user input with "..."
     user_input = user_input.replace(' ','...')
+
+    # Print the user input after replacement
     print(user_input)
 
 if __name__ == "__main__":
