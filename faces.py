a080df20dcc2482a73a07ceaf9d6f44dd8b6799c
Lavanya08M <Lavanya08M@github.com>
2024-12-26 19:45:30 -0500

    Added comments to faces.py

--- faces.py
+++ faces.py
@@ -1,13 +1,21 @@
 def main():
+    # Ask the user for input
     user_input = input()
+
+    # Call convert function and store the result
     result = convert(user_input)
+
+    # Print the result
     print(result)
 
 def convert(usr_input):
+    # If the user input contain :) emoticon replace it with smiling face emoji
     if ":)" in usr_input:
         usr_input = usr_input.replace(":)", "🙂")
+    # If the user input contain :( emoticon replace it with frowning face emoji
     elif ":(" in usr_input:
         usr_input = usr_input.replace(":(", "🙁")
+    # Do not change input if it does not contain emoticon
     else:
         pass
     return usr_input
