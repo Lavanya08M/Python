d8067fe39986ae893ee0cc099fd31faeca2d964f
Lavanya08M <Lavanya08M@github.com>
2024-12-28 12:45:10 -0500

    Added deep.py file

---
+++ deep.py
@@ -0,0 +1,17 @@
+def main():
+    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+    
+    if is_correct(answer):
+        print("Yes")
+    else:
+        print("No")
+
+def is_correct(ans):
+    match ans:
+        case "42" | "forty-two" | "forty two":
+            return True
+        case _:
+            return False
+
+if __name__ == "__main__":
+    main()
