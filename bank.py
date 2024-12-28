4403a340f47b920d81a8fa4be1e10d8b578d9295
Lavanya08M <139261589+Lavanya08M@users.noreply.github.com>
2024-12-28 13:52:29 -0500

    Update bank.py
    Added comments for the bank.py file

--- bank.py
+++ bank.py
@@ -1,14 +1,17 @@
 def main():
+    # Ask the user to enter greeting
     greeting = input("Greeting: ").strip().lower()
     amount = 0
 
+    # If greeting starts with "hello" amount given is zero 
     if greeting.startswith("hello"):
         amount = 0
-    elif greeting.startswith("h"):
+    elif greeting.startswith("h"): # If greeting starts with "h" amount given is 20 dollars
         amount = 20
-    else:
+    else: # If greeting does not start with "hello" or "h" amount given is 100 dollars
         amount = 100
-    
+
+    # Print the amount
     print(f"${amount}")
 
 if __name__ == "__main__":
