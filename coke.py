1b10ef1cfef1fc779b1504edf0569a347a1c878a
Lavanya08M <Lavanya08M@github.com>
2024-12-30 13:03:59 -0500

    Added coke.py file

---
+++ coke.py
@@ -0,0 +1,20 @@
+def main():
+    amount_due = 50
+    total_amount_inserted = 0
+    while True:
+        try:
+            if amount_due > 0:
+                print(f"Amount Due: {amount_due}")
+                insert_coin = int(input("Insert Coin:"))
+                if insert_coin in [25, 10, 5]:
+                    amount_due -= insert_coin
+                    total_amount_inserted += insert_coin
+            elif amount_due <= 0:
+                change = total_amount_inserted - 50
+                print(f"Change Owed: {change}")
+                break
+        except ValueError:
+            continue
+
+if __name__ == "__main__":
+    main()
